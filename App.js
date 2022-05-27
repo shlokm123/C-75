5fe10da1a85e0687fb05d027934287fecb45df15
Shlok Mukane <89182410+shlokm123@users.noreply.github.com>
2022-05-27 19:03:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    //add the return satement for BottomTabNavigator- challenge 4
+    return(
+      <BottomTabNavigator/>
+    )
+
+    
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-75
+# wily-v2-PRO-C68
+Solution code for PRO-C68
